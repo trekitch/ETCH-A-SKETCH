52b27023aa37d95f1a19acdd2f9e12103233fe52
trekitch <trekitch@ymail.com>
2022-01-19 01:45:28 -0500

    intial commit for project

---
+++ js/javascript.js
@@ -0,0 +1,44 @@
+const container = document.querySelector('#container');
+
+
+//creates the grid
+function makeGrid(){
+    let rowtot = 64;
+    let celltot = rowtot * rowtot;
+    
+    container.style.display = 'grid';
+    container.style.gridTemplateRows = `repeat(${rowtot}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${rowtot}, 1fr)`;
+    
+    
+    
+    let row = 1;
+    let column = 1;
+    for (let i = 1; i <= celltot; i++) {
+      let cell = document.createElement('div');
+      cell.classList.add('grid-item')
+      cell.style.border = '1px solid black';
+      cell.style.gridRow = row;
+      cell.style.gridColumn = column;
+      column += 1;
+      if (column === rowtot + 1) {
+        row += 1;
+        column = 1;
+      }
+      container.appendChild(cell);
+    }
+    
+}
+
+makeGrid();
+
+
+const griditems = document.querySelectorAll('.grid-item');
+
+griditems.forEach(griditem => {
+    griditem.addEventListener('mouseover', function(event){
+        event.target.style.backgroundColor = 'black';
+    });
+})
+
+
