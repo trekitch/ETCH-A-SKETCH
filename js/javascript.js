33bf6a542a084d823b435a8d7e5b62092f9fb612
trekitch <trekitch@ymail.com>
2022-01-19 23:18:52 -0500

    added makegrid and gridcolor functions

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,24 @@
 const container = document.querySelector('.container');
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = '#000000';
 
+const selectedColor = document.querySelector('#gridcolor');
+const slider = document.querySelector('#gridsize');
+let slideValue = document.querySelector('#demo');
+
+slideValue.innerHTML = slider.value;
+
+
+makeGrid(DEFAULT_SIZE);
+gridColor(DEFAULT_COLOR);
 
 //creates the grid
-function makeGrid(){
-    let rowtot = 64;
-    let celltot = rowtot * rowtot;
+function makeGrid(gridsize){
+    
+    let celltot = gridsize * gridsize;
 
-    container.style.gridTemplateRows = `repeat(${rowtot}, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(${rowtot}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridsize}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${gridsize}, 1fr)`;
     
     
     
@@ -19,7 +30,7 @@ function makeGrid(){
       cell.style.gridRow = row;
       cell.style.gridColumn = column;
       column += 1;
-      if (column === rowtot + 1) {
+      if (column === gridsize + 1) {
         row += 1;
         column = 1;
       }
@@ -28,15 +39,20 @@ function makeGrid(){
     
 }
 
-makeGrid();
-
-
-const griditems = document.querySelectorAll('.grid-item');
-
-griditems.forEach(griditem => {
+function gridColor(color){
+  const griditems = document.querySelectorAll('.grid-item');
+  griditems.forEach(griditem => {
     griditem.addEventListener('mouseover', function(event){
-        event.target.style.backgroundColor = 'black';
+        event.target.style.backgroundColor = color;
     });
+  });
+}
+
+selectedColor.addEventListener('input', function(e){
+  gridColor(this.value);
 })
 
-
+slider.addEventListener('input', () =>{
+  slideValue.innerHTML = slider.value;
+  makeGrid(+slider.value)
+})
