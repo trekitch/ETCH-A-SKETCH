df2619fa3c9baa79d833161e4ee3032f159da172
trekitch <trekitch@ymail.com>
2022-01-20 21:48:24 -0500

    added logic for erase, reset and resize buttons

--- js/javascript.js
+++ js/javascript.js
@@ -2,25 +2,29 @@ const container = document.querySelector('.container');
 const DEFAULT_SIZE = 16;
 const DEFAULT_COLOR = '#000000';
 
+const resetButton = document.querySelector('.reset');
+const eraseButton = document.querySelector('.erase');
 const selectedColor = document.querySelector('#gridcolor');
 const slider = document.querySelector('#gridsize');
 let slideValue = document.querySelector('#demo');
+let gridItems
 
-slideValue.innerHTML = slider.value;
-
+slideValue.innerHTML = `${slider.value} x ${slider.value}`;
 
 makeGrid(DEFAULT_SIZE);
-gridColor(DEFAULT_COLOR);
+
 
 //creates the grid
 function makeGrid(gridsize){
     
+    //clears out the container if already populated
+    container.innerHTML ='';
+
+    //sets cell total to gridsize squared
     let celltot = gridsize * gridsize;
 
     container.style.gridTemplateRows = `repeat(${gridsize}, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(${gridsize}, 1fr)`;
-    
-    
+    container.style.gridTemplateColumns = `repeat(${gridsize}, 1fr)`;   
     
     let row = 1;
     let column = 1;
@@ -36,23 +40,44 @@ function makeGrid(gridsize){
       }
       container.appendChild(cell);
     }
-    
+
+    if(selectedColor.value == DEFAULT_COLOR){
+      gridColor(DEFAULT_COLOR);
+    }else{
+      gridColor(selectedColor.value);
+    }
+
 }
 
-function gridColor(color){
-  const griditems = document.querySelectorAll('.grid-item');
-  griditems.forEach(griditem => {
+function gridColor(color){  
+  gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach(griditem => {
     griditem.addEventListener('mouseover', function(event){
         event.target.style.backgroundColor = color;
     });
   });
 }
 
-selectedColor.addEventListener('input', function(e){
+selectedColor.addEventListener('input', function(){
   gridColor(this.value);
 })
 
+
 slider.addEventListener('input', () =>{
-  slideValue.innerHTML = slider.value;
-  makeGrid(+slider.value)
+  slideValue.innerHTML = `${slider.value} x ${slider.value}`;
+  makeGrid(+slider.value);
 })
+
+resetButton.onclick = () => {
+  gridItems.forEach(griditem => {
+    griditem.style.backgroundColor = '#FFFFFF'
+  });
+};
+
+eraseButton.onclick = () =>{
+  gridItems.forEach(griditem => {
+    griditem.addEventListener('mouseover', function(event){
+        event.target.style.backgroundColor = '#FFFFFF';
+    });
+  });
+}
